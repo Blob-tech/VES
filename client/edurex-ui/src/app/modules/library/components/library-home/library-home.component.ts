b6a1dfb4100c69e076129f7c5b7a29a6c8fbc49f
agnichndr <55136630+agnichndr@users.noreply.github.com>
2021-03-17 11:46:27 +0530

    Handling session storage for current profile

--- client/edurex-ui/src/app/modules/library/components/library-home/library-home.component.ts
+++ client/edurex-ui/src/app/modules/library/components/library-home/library-home.component.ts
@@ -2,19 +2,18 @@ import { Component, OnInit, DoCheck } from '@angular/core';
 import { LocalStorageService } from 'src/app/shared/services/local-storage.service';
 import { config } from 'src/conf';
 import { SessionStorageService } from 'src/app/shared/services/session-storage.service';
-import { InstituteManagementService } from '../../service/institute-management.service';
 
 @Component({
   selector: 'app-library-home',
   templateUrl: './library-home.component.html',
   styleUrls: ['./library-home.component.css']
 })
-export class LibraryHomeComponent implements OnInit {
+export class LibraryHomeComponent implements OnInit,DoCheck {
 
   ins;
   insUrl = config.host + 'organisation_logo/';
   showLoader = true;
-  constructor(private sessionStorageService : SessionStorageService,private instituteService : InstituteManagementService) { }
+  constructor(private sessionStorageService : SessionStorageService) { }
 
   ngOnInit(): void {
 
@@ -23,11 +22,10 @@ export class LibraryHomeComponent implements OnInit {
 
   }
 
-  // ngDoCheck()
-  // {
-  //  this.ins = this.sessionStorageService.getter('current_institute');
-  // }
- 
-  
+  ngDoCheck()
+  {
+    this.ins = this.sessionStorageService.getter('current_institute');
+  }
+
 
 }
