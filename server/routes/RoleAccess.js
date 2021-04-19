4c94e9f3b2a0ed256e18dcf8fc466d00c7a3d3ca
agnichndr <55136630+agnichndr@users.noreply.github.com>
2021-04-19 22:39:44 +0530

    Role Access for System Admin

--- server/routes/RoleAccess.js
+++ server/routes/RoleAccess.js
@@ -9,22 +9,11 @@ const Role = require('../models/RoleAccess');
 const Organisation = require('../models/Organisation');
 roles.use(cors());
 
-roles.put('/remove_access/:institute_id/:user_id',(req,res,next)=>{
-    Role.deleteOne({$and : [{institute_id : req.params.institute_id},{user_id:req.params.user_id}] }).then(
-        data=>{
-            res.json({"msg" : "Access has been removed permanently"})
-        }
-    ).catch(err=>{
-        res.json({"err" : "Server Error Occured !"+err})
-    })
-})
-
 roles.post('/access/add',(req,res,next)=>{
     let users = req.body.users.split(',');
     let institutes = req.body.institutes.split(',');
     let role = req.body.role;
     let valid_upto = req.body.valid_upto;
-    let approval = req.body.approval;
 
     users.forEach(user => {
         institutes.forEach(institute => {
@@ -50,7 +39,6 @@ roles.post('/access/add',(req,res,next)=>{
                     valid_upto : valid_upto,
                     active : true,
                     is_activated : false,
-                    approval : approval
                 }
             },
             {
@@ -92,47 +80,6 @@ roles.put('/toggle_access/:user_id/:institute_id/:access',(req,res,next)=>{
     }
 })
 
-roles.put('/approve_access',(req,res,next)=>
-{
-    if(req.body.institute_id == 'SADMIN')
-    {
-        Role.findOneAndUpdate(
-            {$and : 
-                [
-                    {active:true},
-                    {is_activated : false},
-                    {user_id : req.body.user_id},
-                    {role : req.body.institute_id},
-                    {approval : req.body.approver}]},
-                    {$set : {
-                    is_activated : true,
-                    approval : "approved",
-                    valid_upto : req.body.valid_upto,
-                    }}).then(data=>{
-                res.json({"msg" : "Access Approved"})
-            }).catch(err=>{
-                res.json({"err" : err})
-            });
-    }
-    else{
-    Role.findOneAndUpdate(
-        {$and : [{active:true},
-            {is_activated : false},
-            {user_id : req.body.user_id},
-            {institute_id : req.body.institute_id},
-            {approval : req.body.approver}]},
-    {$set : {
-        is_activated : true,
-        approval : "approved",
-        valid_upto : req.body.valid_upto,
-        role : req.body.role
-    }}).then(data=>{
-        res.json({"msg" : "Access Approved"})
-    }).catch(err=>{
-        res.json({"err" : err})
-    });}
-})
-
 roles.put('/toggle_system_access/:user_id/:access',(req,res,next)=>{
     if(req.params.access == 'revoke')
     {
@@ -159,20 +106,7 @@ roles.put('/toggle_system_access/:user_id/:access',(req,res,next)=>{
     }
 })
 
-roles.get('/people_count/:institute_id/:role',(req,res,next)=>{
 
-    Role.find({$and : [{active : true},{is_activated : true},{institute_id : req.params.institute_id}
-    ,{role : req.params.role}]}).countDocuments((err,count)=>{
-        if(err)
-        {
-            res.json({"err" : "Server Error ! Error in retrieving count of" + req.params.role});
-        }
-        else
-        {
-            res.json(count);
-        }
-    })
-})
 roles.get('/individual_access/:user_id/:institute_id',(req,res,next)=>{
     Role.findOne({$and : [{user_id : req.params.user_id},{institute_id : req.params.institute_id}]}
         )
@@ -201,67 +135,6 @@ roles.get('/system_access/:user_id',(req,res,next)=>{
 roles.get('/institute/access/:user_id',(req,res,next)=>{
     let role;
     Role.find({$and : [{active : true},{user_id : req.params.user_id}]}).then(
-        data=>{
-            role=data;
-            data = data.map(value =>{
-               return  value.institute_id
-            });
-            Organisation.find({$and : [{active : true},{isActivated : true},{organisation_id : {$in : data }}]})
-            .sort({organisation_name : 1})
-            .then(
-                result=>{
-                    res.json({"role" : role, "ins" : result});
-                }
-
-            ).catch(err=>{
-                res.json({"err" : "Error in loading institute list" + err});
-            })
-
-            
-        }
-    ).catch(err=>{
-        res.json({"err" : "Error in loading institute list" + err});
-    })
-
-})
-
-roles.get('/institute/access/:user_id',(req,res,next)=>{
-    let role;
-    Role.find({$and : [{active : true},{user_id : req.params.user_id}]}).then(
-        data=>{
-            role=data;
-            data = data.map(value =>{
-               return  value.institute_id
-            });
-            Organisation.find({$and : [{active : true},{isActivated : true},{organisation_id : {$in : data }}]})
-            .sort({organisation_name : 1})
-            .then(
-                result=>{
-                    res.json({"role" : role, "ins" : result});
-                }
-
-            ).catch(err=>{
-                res.json({"err" : "Error in loading institute list" + err});
-            })
-
-            
-        }
-    ).catch(err=>{
-        res.json({"err" : "Error in loading institute list" + err});
-    })
-
-})
-
-roles.get('/active_institute/access/:user_id',(req,res,next)=>{
-    let role;
-    Role.find({$and : 
-        [{active : true},
-        {user_id : req.params.user_id},
-        {is_activated : true},
-        {approval : 'approved'},
-        {$or : [{valid_upto:{$gte : new Date()}},{valid_upto : ''},{valid_upto : null}]}
-    ]}
-    ).then(
         data=>{
             role=data;
             data = data.map(value =>{
