e2514b86e8de2390d93c21b26d783053ca2e2a91
agnichndr <55136630+agnichndr@users.noreply.github.com>
2021-04-25 23:47:08 +0530

    Enhanced Role Management Tool

--- client/edurex-ui/src/app/modules/library/modules/institute-management/components/create-institute/create-institute.component.ts
+++ client/edurex-ui/src/app/modules/library/modules/institute-management/components/create-institute/create-institute.component.ts
@@ -9,6 +9,7 @@ import { Router } from '@angular/router';
 import { LibraryCategoryService } from 'src/app/modules/library/service/library-category.service';
 import { SubscriberService } from 'src/app/modules/library/service/subscriber.service';
 import { InstituteManagementService } from 'src/app/modules/library/service/institute-management.service';
+import { RoleAccessService } from 'src/app/shared/services/role-access.service';
 
 
 
@@ -37,7 +38,8 @@ export class CreateInstituteComponent implements OnInit,OnChanges {
   url = "assets/images/user.png";
   constructor(private formBuilder : FormBuilder, private libCategoryServices : LibraryCategoryService,
     private _snackbar : MatSnackBar, private router : Router,private instituteService :InstituteManagementService,
-    private navbar : NavbarService, private subscriberService : SubscriberService) { }
+    private navbar : NavbarService, private subscriberService : SubscriberService,
+    private roleAccessService : RoleAccessService) { }
 
     
   ngOnInit(): void {
@@ -222,13 +224,12 @@ export class CreateInstituteComponent implements OnInit,OnChanges {
   {
     let userformData = new FormData()
     userformData.append('avatar',this.imageFile);
-    userformData.append('user_id' ,this.client_id.value());
-    userformData.append('email' ,this.contact_email.value());
-    userformData.append('phone' ,this.contact_phone.value());
-    userformData.append('name' ,this.contact_person.value());
-    userformData.append('address' ,this.address.value());
-    userformData.append('password' ,this.contact_phone.value());
-    
+    userformData.append('user_id' ,this.client_id.value);
+    userformData.append('email' ,this.contact_email.value);
+    userformData.append('phone' ,this.contact_phone.value);
+    userformData.append('name' ,this.contact_person.value);
+    userformData.append('address' ,this.address.value);
+    userformData.append('password' ,this.contact_phone.value);
 
     this.subscriberService.register(userformData).subscribe(
       data=>
@@ -237,25 +238,63 @@ export class CreateInstituteComponent implements OnInit,OnChanges {
         if(!(JSON.parse(JSON.stringify(data))['err']))
         {
           
-          this.error = null;
+          this.instituteAdminAccess();
         }
         else
         {
           this.message = null;
           this.error =  JSON.parse(JSON.stringify(data))['err'];
-         
+          this.showLoader = false;
         }
       },
       err =>
       {
         this.message = null;
         this.error = "Error in adding new subscriber to Edurex Database. Please try after few minutes.";
+        this.showLoader =false;
         
       }
     )
     
   }
 
+
+  instituteAdminAccess()
+  {
+      let formData = new FormData();
+      let validupto : Date;
+     
+      formData.append('users', this.client_id.value);
+      formData.append('institutes',this.organisation_id.value);
+      formData.append('role','IADMIN');
+      formData.append('valid_upto', '');
+      this.roleAccessService.giveRoleAccess(formData).subscribe(
+        data=>{
+          if(!(JSON.parse(JSON.stringify(data))['err']))
+          {
+            this.error = null;
+            this.message = JSON.parse(JSON.stringify(data))['msg'];
+            this._snackbar.open(JSON.parse(JSON.stringify(data))['msg'],null,{duration:5000})
+            this.router.navigateByUrl('e-library/institute/institute-management/list/all').then(
+              ()=>{
+                this.showLoader = false;
+              }
+            );
+            
+          }
+          else
+          {
+            this._snackbar.open(JSON.parse(JSON.stringify(data))['err'],null,{duration : 5000});
+            this.showLoader=false;
+          }
+        },
+        err=>{
+          this._snackbar.open("Error in Giving Access ! Please try after few minutes", null , {duration : 5000} );
+          this.showLoader=false;
+        }
+      )
+    
+  }
   
 
   register()
@@ -277,18 +316,9 @@ export class CreateInstituteComponent implements OnInit,OnChanges {
         
         if(!(JSON.parse(JSON.stringify(data))['err']))
         {
-          //this.registerInstituteAdmin();
-          this.error = null;
-          this.message = JSON.parse(JSON.stringify(data))['msg'];
-          this._snackbar.open(JSON.parse(JSON.stringify(data))['msg'],null,{duration:5000})
-          this.router.navigateByUrl('e-library/institute/institute-management/list/all').then(
-            ()=>{
-              this.showLoader = false;
-            }
-          );
+          this.registerInstituteAdmin();
+         
         }
-        
-        
         {
           this.message = null;
           this.error =  JSON.parse(JSON.stringify(data))['err'];
